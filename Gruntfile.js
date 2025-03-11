dca8bdf146eb8ee4c58080d1f5b07c526291e67d
sqbl <18955628948@163.com>
2025-03-11 14:28:45 +0800

    修改配置

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,9 @@ module.exports = function(grunt) {
     grunt.initConfig({
         screeps: {
             options: {
-                email: '2673397594@qq.com',
-                token: 'd231e466-8559-465b-b98b-3319c22df738',
-                branch: 'default',
+                email: '<your email>',
+                token: 'your token',
+                branch: 'your branch',
                 //server: 'season'
             },
             dist: {
